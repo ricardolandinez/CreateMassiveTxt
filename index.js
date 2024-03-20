6b8a4d8988fb0426b99157866012d7a84645da3b
ricardo <ricardo20-@hotmail.com>
2024-03-20 17:59:23 -0500

    DocumentationJsDocs

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-// Asumiendo que tienes una ruta específica para el archivo de entrada
+/**
+ * Ruta absoluta del archivo de entrada desde el cual se generarán las modificaciones.
+ * @type {string}
+ */
 const inputFilePath = `C:\\Users\\Ricardo A Landinez\\Downloads\\NuevosDocumentos\\DocumentoPruebaMasivaEsdinamicoAEsdinamico.txt`;
-// Y una ruta específica para guardar los archivos modificados
+
+/**
+ * Ruta absoluta del directorio donde se guardarán los archivos modificados.
+ * @type {string}
+ */
 const outputDirectoryPath = `C:\\Users\\Ricardo A Landinez\\Downloads\\NuevosDocumentos\\DocumentosModificados`;
 
 // Crear el directorio de salida si no existe
@@ -11,7 +18,10 @@ if (!fs.existsSync(outputDirectoryPath)) {
   fs.mkdirSync(outputDirectoryPath);
 }
 
-// Leer el archivo de entrada
+/**
+ * Lee un archivo de entrada, modifica su contenido incrementando un número en una cadena específica,
+ * y genera 1000 archivos nuevos con el contenido modificado.
+ */
 fs.readFile(inputFilePath, 'utf8', (err, data) => {
   if (err) {
     console.error('Error al leer el archivo:', err);
@@ -20,7 +30,7 @@ fs.readFile(inputFilePath, 'utf8', (err, data) => {
 
   // Generar 1000 archivos modificados
   for (let i = 1; i <= 1000; i++) {
-    // Proceso similar al anterior, pero con un incremento por cada archivo
+    // Descompone el contenido del archivo en líneas, las modifica si es necesario, y luego las recompone
     let lines = data.split('\n');
     lines = lines.map((line) => {
       if (line.startsWith('01|')) {
@@ -28,7 +38,7 @@ fs.readFile(inputFilePath, 'utf8', (err, data) => {
         if (parts[3].startsWith('SETP')) {
           const prefix = parts[3].substring(0, 4); // 'SETP'
           const number = parseInt(parts[3].substring(4), 10) + i; // Incrementa el número según el índice del bucle
-          parts[3] = prefix + number; // Reconstruye la parte con el nuevo número
+          parts[3] = prefix + number.toString(); // Reconstruye la parte con el nuevo número
         }
         return parts.join('|');
       } else {
@@ -39,7 +49,7 @@ fs.readFile(inputFilePath, 'utf8', (err, data) => {
     const modifiedData = lines.join('\n');
     const newFilePath = path.join(outputDirectoryPath, `DocumentoModificado_${i}.txt`);
 
-    // Escribir cada archivo modificado
+    // Escribir cada archivo modificado en el sistema de archivos
     fs.writeFile(newFilePath, modifiedData, 'utf8', (err) => {
       if (err) {
         console.error(`Error al escribir el archivo ${i}:`, err);
